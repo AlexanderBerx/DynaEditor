9b09f1552df33204d67d4fa22a8c943e813c981a
Alexander Berx <alexanderberx@gmail.com>
2017-10-21 11:02:01 +0200

    splited attr args in seperated vars in const module

--- dynaeditor/const.py
+++ dynaeditor/const.py
@@ -1,14 +1,25 @@
 # argument keys
-ARG_KEYS = ["_type",
-            "attr",
-            "nice_name",
-            "_min",
-            "_max",
-            "default_value",
-            "options",
-            "file_path",
-            "color",
-            "categories"]
+ATTR_ARG_TYPE = "_type"
+ATTR_ARG_ATTR = "attr"
+ATTR_ARG_NICE_NAME = "nice_name"
+ATTR_ARG_MIN = "_min"
+ATTR_ARG_MAX = "_max"
+ATTR_ARG_DEFAULT_VALUE = "default_value"
+ATTR_ARG_OPTIONS = "options"
+ATTR_ARG_FILE_PATH = "file_path"
+ATTR_ARG_COLOR = "color"
+ATTR_ARG_CATEGORIES = "categories"
+
+ARG_KEYS = [ATTR_ARG_TYPE,
+            ATTR_ARG_ATTR,
+            ATTR_ARG_NICE_NAME,
+            ATTR_ARG_MIN,
+            ATTR_ARG_MAX,
+            ATTR_ARG_DEFAULT_VALUE,
+            ATTR_ARG_OPTIONS,
+            ATTR_ARG_FILE_PATH,
+            ATTR_ARG_COLOR,
+            ATTR_ARG_CATEGORIES]
 
 # attribute types
 ATYPE_BOOL = "bool"
