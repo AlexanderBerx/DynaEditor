82ff2fb88d27374dadb7cb88ba809494ca66092b
DESKTOP-LE3UG9F\tabletos <alexander@cyborn.be>
2017-10-25 07:32:38 +0200

    extended base widget functionallity to be able to emit value signals

--- dynaeditor/widgets/base_widget.py
+++ dynaeditor/widgets/base_widget.py
@@ -1,11 +1,15 @@
-from PySide2 import QtWidgets
+import json
+from PySide2 import QtWidgets, QtCore
 
 
 class BaseWidget(QtWidgets.QWidget):
+    signal_apply_attr = QtCore.Signal(str)
+
     def __init__(self, nice_name, default_value):
         super(BaseWidget, self).__init__()
         self._create_ui(nice_name)
         self.set_default_value(default_value)
+        self.connect_signals()
 
     def _create_ui(self, nice_name):
         layout_main = QtWidgets.QGridLayout()
@@ -14,8 +18,8 @@ class BaseWidget(QtWidgets.QWidget):
         layout_main.addWidget(QtWidgets.QLabel(nice_name))
         layout_main.addWidget(self.create_type_widget(), 0, 1)
 
-        btn_set = QtWidgets.QPushButton("Set")
-        layout_main.addWidget(btn_set, 0, 2)
+        self._btn_set = QtWidgets.QPushButton("Set")
+        layout_main.addWidget(self._btn_set, 0, 2)
 
     def create_type_widget(self):
         """
@@ -32,3 +36,26 @@ class BaseWidget(QtWidgets.QWidget):
         :return:
         """
         raise NotImplementedError("Method must be overwritten in child Classes")
+
+    def get_value(self):
+        """
+        returns the current value of the widget
+        :return: object
+        """
+        raise NotImplementedError("no get value method implemented")
+
+    def _emit_attr(self):
+        """
+        emits the current value of the widget as a string
+        :return: None
+        """
+        value = self.get_value()
+        value = json.dumps(value)
+        self.signal_apply_attr.emit(value)
+
+    def connect_signals(self):
+        """
+        connect the signals of the widget
+        :return: None
+        """
+        self._btn_set.clicked.connect(self._emit_attr)
