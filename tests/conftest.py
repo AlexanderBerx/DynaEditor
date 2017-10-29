b802f1ca702312f32e75937dc431ecf703b8750a
Alexander Berx <alexanderberx@gmail.com>
2017-10-29 14:27:39 +0100

    added test_widgets module to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
-from PySide2 import QtWidgets
+import os
 try:
     from maya import standalone
 except ImportError:
-    raise EnvironmentError("Can't run tests due to no maya modules present in current environment")
-
+    standalone = None
+    raise ImportError("Can't run tests due to no maya modules present in current environment")
+try:
+    from PySide2 import QtWidgets
+except ImportError:
+    raise ImportError("Can't run tests due to no PySide2 present in current environment")
 
 def pytest_sessionstart(session):
     """ before session.main() is called. """
+    # skips all the usersetup.py modules
+    os.environ['MAYA_SKIP_USERSETUP_PY'] = '1'
     # NOTE: the QApplication has to be created before maya is initialized
     QtWidgets.QApplication([])
     standalone.initialize()
