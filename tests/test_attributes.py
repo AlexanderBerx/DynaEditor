43b61f577fc744db6a3139704fe314d667343405
DESKTOP-LE3UG9F\tabletos <alexander@cyborn.be>
2017-10-30 07:46:46 +0100

    added unit test for BoolAttribute class

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -1,4 +1,7 @@
+import json
 import pytest
+from PySide2 import QtCore
+from dynaeditor import widgets
 from dynaeditor import attributes
 
 
@@ -13,4 +16,31 @@ def test_base_attribute():
 
 
 def test_bool_attribute():
-    pass
+    """
+    tests the BoolAttribute class
+    :return: None
+    """
+    attr = "test_bool"
+    nice_name = "test bool"
+    bool_type = "bool"
+    default_value = False
+    bool_attr = attributes.BoolAttribute(attr, nice_name, default_value)
+    # connect to the attr slot for signal testing
+    @QtCore.Slot(str, str, str)
+    def attr_slot(name, type_, value):
+        assert name == attr
+        assert type_ == bool_type
+        assert json.loads(value) == default_value
+
+    bool_attr.signal_apply_attr[str, str, str].connect(attr_slot)
+
+    # check if the widget type is correct
+    assert type(bool_attr.widget) == widgets.BoolWidget
+    # check the type
+    assert bool_attr.type_ == bool_type
+    # check if the name was set correctly
+    assert bool_attr.name == attr
+    # test signals
+    bool_attr.widget._emit_attr()
+
+
