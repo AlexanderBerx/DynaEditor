e8cf1f20e0e961d546cf097bc4a82249196a079d
DESKTOP-LE3UG9F\tabletos <alexander@cyborn.be>
2017-11-02 07:27:57 +0100

    added test data to app testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+import os
+import json
+from dynaeditor.utils import general_utils
 from dynaeditor.controller import Editor
 
 
@@ -6,4 +9,11 @@ def test_app():
     tests if the whole app runs
     :return:
     """
-    Editor()
+    editor = Editor()
+    test_data = os.path.split(__file__)[0]
+    test_data = os.path.join(test_data, "../rsc/test_data.json")
+    test_data = os.path.abspath(test_data)
+    with open(test_data, 'r') as file_in:
+        data = json.load(file_in)
+    mapped_data = [general_utils.key_map_config(item) for item in data]
+    editor.set_editor_options(mapped_data)
