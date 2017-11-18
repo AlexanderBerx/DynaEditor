6d91d9d880de80b2dd3199a6d1453d0d998f236e
Alexander Berx <alexanderberx@gmail.com>
2017-11-18 22:49:27 +0100

    updated attribute tests

---
+++ dynaeditor/attribute_widgets/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['base_widget', 'bool_widget', 'enum_widget', 'float3_color_widget', 'float3_widget', 'float_widget']
+
+from dynaeditor.attribute_widgets.base_widget import BaseWidget
+from dynaeditor.attribute_widgets.bool_widget import BoolWidget
+from dynaeditor.attribute_widgets.enum_widget import EnumWidget
+from dynaeditor.attribute_widgets.float3_color_widget import Float3ColorWidget
+from dynaeditor.attribute_widgets.float3_widget import Float3Widget
--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -1,10 +1,9 @@
-import json
 import pytest
 try:
     from PySide2 import QtCore
 except ImportError:
     from Qt import QtCore
-from dynaeditor import widgets
+from dynaeditor import attribute_widgets
 from dynaeditor import attributes
 
 
@@ -14,122 +13,58 @@ def test_base_attribute():
     :return: None
     """
     # test if abstraction is being enforced
+    attribute = attributes.BaseAttribute("test", "some_attr")
     with pytest.raises(NotImplementedError):
-        attributes.BaseAttribute("test", None, "some_attr")
+        attribute._create_widget()
+
+def test_attribute():
+    # attribute class needs to raise an type error if an type is not known to it
+    with pytest.raises(TypeError):
+        attributes.Attribute(_type="unknown")
 
 
 def test_bool_attribute():
-    """
-    tests the BoolAttribute class
-    :return: None
-    """
     attr = "test_bool"
     nice_name = "test bool"
-    bool_type = "bool"
     default_value = False
     bool_attr = attributes.BoolAttribute(attr, nice_name, default_value)
 
-    # connect to the attr slot for signal testing
-    @QtCore.Slot(str, str, str)
-    def attr_slot(name, type_, value):
-        assert name == attr
-        assert type_ == bool_type
-        # since the value isn't being changed in the test the emited
-        # value needs to still match the default value
-        assert json.loads(value) == default_value
-
-    bool_attr.signal_apply_attr[str, str, str].connect(attr_slot)
 
     # check if the widget type is correct
-    assert type(bool_attr.widget) == widgets.BoolWidget
+    assert type(bool_attr.widget) == attribute_widgets.BoolWidget
     # check the type
-    assert bool_attr.data_type == bool_type
+    assert bool_attr.data_type == ""
     # check if the name was set correctly
     assert bool_attr.name == attr
-    # test signals
-    bool_attr.widget._emit_attr()
-    # test arg validation
-    assert attributes.BoolAttribute.validate_args(attr=attr, nice_name=nice_name) is True
-    assert attributes.BoolAttribute.validate_args(attr=attr) is False
-    assert attributes.BoolAttribute.validate_args(nice_name=nice_name) is False
 
 
 def test_enum_attribute():
-    """
-    tests the EnumAttribute class
-    :return: None
-    """
     attr = "test_enum"
     nice_name = "test enum"
-    enum_type = "enum"
+    enum_type = ""
     default_value = 2
     test_options = ["1", "2", "3", "4"]
     enum_attr = attributes.EnumAttribute(attr, nice_name, default_value, test_options)
 
-    # connect to the attr slot for signal testing
-    @QtCore.Slot(str, str, str)
-    def attr_slot(name, type_, value):
-        assert name == attr
-        assert type_ == enum_type
-        # since the value isn't being changed in the test the emited
-        # value needs to still match the default value
-        assert json.loads(value) == default_value
-
-    enum_attr.signal_apply_attr[str, str, str].connect(attr_slot)
-
     # check if the widget type is correct
-    assert type(enum_attr.widget) == widgets.EnumWidget
+    assert type(enum_attr.widget) == attribute_widgets.EnumWidget
     # check the type
     assert enum_attr.data_type == enum_type
     # check if the name was set correctly
     assert enum_attr.name == attr
-    # test signals
-    enum_attr.widget._emit_attr()
-    # test arg validation
-    assert attributes.EnumAttribute.validate_args(attr=attr,
-                                                  nice_name=nice_name,
-                                                  default_value=default_value,
-                                                  options=test_options) is True
-    assert attributes.EnumAttribute.validate_args(attr=attr) is False
 
 
 def test_float3_attribute():
-    """
-    tests the Float3Attribute class
-    :return: None
-    """
     attr = "test_float3"
     nice_name = "test float3"
     float3_type = "float3"
     default_value = [0.0, 0.0, 1.0]
     enum_attr = attributes.Float3Attribute(attr, nice_name, default_value)
 
-    # connect to the attr slot for signal testing
-    @QtCore.Slot(str, str, str)
-    def attr_slot(name, type_, value):
-        assert name == attr
-        assert type_ == float3_type
-        # since the value isn't being changed in the test the emited
-        # value needs to still match the default value
-        assert json.loads(value) == default_value
-
-    enum_attr.signal_apply_attr[str, str, str].connect(attr_slot)
-
     # check if the widget type is correct
-    assert type(enum_attr.widget) == widgets.Float3Widget
+    assert type(enum_attr.widget) == attribute_widgets.Float3Widget
     # check the type
     assert enum_attr.data_type == float3_type
     # check if the name was set correctly
     assert enum_attr.name == attr
-    # test signals
-    enum_attr.widget._emit_attr()
 
-
-def test_attribute():
-    """
-    tests the Attribute class
-    :return: None
-    """
-    # attribute class needs to raise an type error if an type is not known to it
-    with pytest.raises(TypeError):
-        attributes.Attribute(_type="unknown")
