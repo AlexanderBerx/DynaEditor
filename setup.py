f5af6c89daa0edebf317af9d49f4792cae88bddd
Alexander Berx <alexanderberx@gmail.com>
2017-11-18 21:24:01 +0100

    updated setup.py accordingly

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='DynaEditor',
     version='v0.1.0',
-    packages=['dynaeditor', 'dynaeditor.utils', 'dynaeditor.widgets', 'dynaeditor.attributes'],
-    url='https://github.com/AlexanderBerx/DynaEditor',
-    license='BSD-3-Clause',
-    author='Alexander',
+    packages=['dynaeditor', 'dynaeditor.utils', 'dynaeditor.widgets', 'dynaeditor.attributes',
+              'dynaeditor.attribute_widgets'],
+    url='',
+    license='BSD3',
+    author='Alexander Berx',
     author_email='alexanderberx@gmail.com',
-    description='Dynamic attribute editor for maya 2017 & up'
+    description='dynamic attribute editor for Autodesk Maya 2017 and up'
 )
